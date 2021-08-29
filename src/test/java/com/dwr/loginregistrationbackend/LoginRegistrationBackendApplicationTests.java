74df1d877998eef43c702dcf69f27ea8927e477b
Dawid.Rym <dawid.jscript@gmail.com>
2021-08-29 12:51:11 +0200

    Initial commit

---
+++ src/test/java/com/dwr/loginregistrationbackend/LoginRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwr.loginregistrationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
